c04794a58ee89e0f7a49f96d86786c9cddeb8877
Gopesh Baheti <GOPESH.21517@student.trinity.edu.np>
2022-12-21 16:20:07 +0545

    fixes

--- problemSolved/settings.py
+++ problemSolved/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'comment_database',
         'USER': 'root',
         'PASSWORD': 'Gopesh',
-        'HOST': 'https://starfish-app-s6435.ondigitalocean.app/home',
+        'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
